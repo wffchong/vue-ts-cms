8d81bf7cda834aef4764bd0e267fc7ba53cf67d8
wffccc <fufanwang@pawnfi.com>
2022-06-07 09:52:01 +0800

    chore(project): element-plus按需加载

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// element-plus按需加载
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            AutoImport({
+                resolvers: [ElementPlusResolver()]
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()]
+            })
+        ]
+    }
 })
