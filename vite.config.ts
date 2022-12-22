f09719db5b302e44f2f446709aa409a30a431db7
wffchong <283953455@qq.com>
2022-12-22 12:53:47 +0800

    chore: 🔨 配置element-plus自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// element-plus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 配置 element-plus 例如el-message等组件自动导入样式
+import { createStyleImportPlugin, ElementPlusResolve } from 'vite-plugin-style-import'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue()]
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+	console.log(mode)
+	return {
+		plugins: [
+			vue(),
+			AutoImport({
+				resolvers: [ElementPlusResolver()]
+			}),
+			Components({
+				resolvers: [ElementPlusResolver()]
+			}),
+			createStyleImportPlugin({
+				resolves: [ElementPlusResolve()],
+				libs: [
+					{
+						libraryName: 'element-plus',
+						esModule: true,
+						resolveStyle: (name: string) => {
+							return `element-plus/theme-chalk/${name}.css`
+						}
+					}
+				]
+			})
+		]
+	}
 })
